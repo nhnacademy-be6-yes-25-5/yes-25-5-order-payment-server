270c1d5f487c36774e100169cd1743dbee5668a2
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-12 23:02:11 +0900

    initial commit

---
+++ src/main/java/com/yes25/yes255orderpaymentserver/Yes255OrderPaymentServerApplication.java
@@ -0,0 +1,13 @@
+package com.yes25.yes255orderpaymentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yes255OrderPaymentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yes255OrderPaymentServerApplication.class, args);
+    }
+
+}
