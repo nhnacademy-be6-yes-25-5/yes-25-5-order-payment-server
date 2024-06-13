af1051e7a643503ee81a415b3398b4b42cf31070
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-13 22:39:00 +0900

    fix : 테스트 코드 통과 안되는 이슈 해결

--- src/test/java/com/yes25/yes255orderpaymentserver/Yes255OrderPaymentServerApplicationTests.java
+++ src/test/java/com/yes25/yes255orderpaymentserver/Yes255OrderPaymentServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.yes25.yes255orderpaymentserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Yes255OrderPaymentServerApplicationTests {
 
     @Test
