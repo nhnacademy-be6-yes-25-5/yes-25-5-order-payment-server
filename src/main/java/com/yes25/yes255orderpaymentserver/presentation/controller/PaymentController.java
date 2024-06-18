0329673c81365486103b5332efd0088c4787f541
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-18 11:17:43 +0900

    feat : 결제 완료 시, 결제 저장 기능 구현함

---
+++ src/main/java/com/yes25/yes255orderpaymentserver/presentation/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.yes25.yes255orderpaymentserver.presentation.controller;
+
+import com.yes25.yes255orderpaymentserver.application.service.PaymentService;
+import com.yes25.yes255orderpaymentserver.presentation.dto.request.CreatePaymentRequest;
+import com.yes25.yes255orderpaymentserver.presentation.dto.response.CreatePaymentResponse;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/confirm")
+    public ResponseEntity<CreatePaymentResponse> confirmPayment(@RequestBody CreatePaymentRequest request) {
+        CreatePaymentResponse response = paymentService.createPayment(request);
+
+        return ResponseEntity.status(response.status())
+            .body(response);
+    }
+}
