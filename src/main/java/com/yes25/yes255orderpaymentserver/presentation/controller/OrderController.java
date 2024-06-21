fbd60bec75394288dde88d023918e8c50c32b145
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-21 13:13:23 +0900

    fix : 포인트 업데이트 요청 시, 의존성 주입 클래스가 null이 되는 현상 해결함

--- src/main/java/com/yes25/yes255orderpaymentserver/presentation/controller/OrderController.java
+++ src/main/java/com/yes25/yes255orderpaymentserver/presentation/controller/OrderController.java
@@ -10,7 +10,6 @@ import com.yes25.yes255orderpaymentserver.presentation.dto.response.ReadUserOrde
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
