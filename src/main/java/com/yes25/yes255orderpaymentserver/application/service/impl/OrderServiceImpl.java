d601033cd04ebd3b7cfd1a08d45b39b3e7f2c74b
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-03 02:00:09 +0900

    chore : 반품 api 구현으로 인한 주석 제거

--- src/main/java/com/yes25/yes255orderpaymentserver/application/service/impl/OrderServiceImpl.java
+++ src/main/java/com/yes25/yes255orderpaymentserver/application/service/impl/OrderServiceImpl.java
@@ -204,7 +204,7 @@ public class OrderServiceImpl implements OrderService {
             UpdateRefundRequest refundRequest = UpdateRefundRequest.from(returnAmount);
 
             // fixme. 포인트 업데이트 API 구현 시, 주석 해제하겠습니다.
-//            userAdaptor.updatePointByRefund(refundRequest);
+            userAdaptor.updatePointByRefund(refundRequest);
 
         } else {
             throw new AccessDeniedException("배송이 시작되지 않은 주문은 반품할 수 없습니다. 주문 ID : " + orderId);
