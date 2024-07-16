de9d0651fdd0c2dde3b944183a111b46b2950250
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-16 19:14:59 +0900

    test : 관리자 테스트 코드 작성

--- src/test/java/com/yes25/yes255orderpaymentserver/presentation/controller/AdminOrderControllerTest.java
+++ src/test/java/com/yes25/yes255orderpaymentserver/presentation/controller/AdminOrderControllerTest.java
@@ -1,6 +1,5 @@
 package com.yes25.yes255orderpaymentserver.presentation.controller;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -10,8 +9,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yes25.yes255orderpaymentserver.application.service.AdminOrderService;
-import com.yes25.yes255orderpaymentserver.common.jwt.JwtFilter;
-import com.yes25.yes255orderpaymentserver.common.jwt.JwtProvider;
 import com.yes25.yes255orderpaymentserver.persistance.domain.enumtype.CancelStatus;
 import com.yes25.yes255orderpaymentserver.persistance.domain.enumtype.OrderStatusType;
 import com.yes25.yes255orderpaymentserver.presentation.dto.request.CancelOrderRequest;
@@ -23,38 +20,41 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest(AdminOrderController.class)
-@WithMockUser
+@ExtendWith(MockitoExtension.class)
 class AdminOrderControllerTest {
 
-    @MockBean
+    @Mock
     private AdminOrderService adminOrderService;
 
-    @MockBean
-    private JwtFilter jwtFilter;
+    @InjectMocks
+    private AdminOrderController adminOrderController;
 
-    @MockBean
-    private JwtProvider jwtProvider;
-
-    @Autowired
     private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
-        when(jwtProvider.getUserNameFromToken(anyString())).thenReturn("user");
+        mockMvc = MockMvcBuilders.standaloneSetup(adminOrderController)
+            .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+            .build();
+        objectMapper = new ObjectMapper();
     }
+
 
     @DisplayName("관리자가 회원 혹은 비회원에 대한 주문 내역을 조회하는지 확인한다.")
     @ParameterizedTest
@@ -71,44 +71,48 @@ class AdminOrderControllerTest {
             .andExpect(status().isOk());
     }
 
+    @DisplayName("잘못된 경로로 요청을 보내면 404 오류가 발생하는지 확인한다.")
+    @Test
+    void invalidUrlReturns404() throws Exception {
+        mockMvc.perform(get("/orders/admins"))
+            .andExpect(status().isNotFound());
+    }
+
     @DisplayName("관리자가 주문 ID로 상태를 업데이트하는지 확인한다.")
     @Test
     void update() throws Exception {
         // given
-        ObjectMapper objectMapper = new ObjectMapper();
         UpdateOrderStatusRequest request = new UpdateOrderStatusRequest(OrderStatusType.CANCEL);
         String requestBody = objectMapper.writeValueAsString(request);
 
         String orderId = "order";
         doNothing().when(adminOrderService).updateOrderStatusByOrderId(orderId, request);
 
-
         // when && then
         mockMvc.perform(put("/orders/admin/" + orderId)
-            .content(requestBody)
-            .with(csrf()))
-            .andExpect(status().isOk());
+                .content(requestBody)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
     }
 
     @DisplayName("관리자가 환불 주문 내역을 조회하는지 확인한다.")
     @Test
     void testGetAllOrders() throws Exception {
         // given
-        Pageable pageable = PageRequest.of(0, 10);
+        Pageable pageable = PageRequest.of(0, 20);
         Page<ReadAllUserOrderCancelStatusResponse> responses = new PageImpl<>(List.of(), pageable, 0);
         when(adminOrderService.getAllCancelOrdersByPaging(pageable)).thenReturn(responses);
 
         // when && then
         mockMvc.perform(get("/orders/admin/refund"))
             .andExpect(status().isOk());
-
     }
 
     @DisplayName("관리자가 주문 ID를 통해 환불을 처리하는지 확인한다.")
     @Test
     void cancelOrder() throws Exception {
         // given
-        ObjectMapper objectMapper = new ObjectMapper();
         CancelOrderRequest request = new CancelOrderRequest(CancelStatus.ACCESS);
         String orderId = "order";
         CancelOrderResponse response = CancelOrderResponse.from(CancelStatus.ACCESS);
@@ -119,8 +123,9 @@ class AdminOrderControllerTest {
 
         // when && then
         mockMvc.perform(put("/orders/admin/" + orderId + "/refund")
-            .content(requestBody)
-            .with(csrf()))
+                .content(requestBody)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
     }
 }
