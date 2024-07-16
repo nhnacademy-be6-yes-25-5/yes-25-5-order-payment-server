4e35b3cff3072bb74f85efa1f7b1dfb34fd04280
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-16 13:13:34 +0900

    docs : secure key manager 설정

---
+++ src/test/java/com/yes25/yes255orderpaymentserver/Yes255OrderPaymentServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yes25.yes255orderpaymentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class Yes255OrderPaymentServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
