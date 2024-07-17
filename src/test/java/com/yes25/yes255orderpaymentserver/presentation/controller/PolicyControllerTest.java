3e04fbbe841952dabf87b6a6c21bf305ec135028
bbbbooo <qlqlzbxkzl@naver.com>
2024-07-17 19:54:16 +0900

    test : 결제 테스트 코드 작성

---
+++ src/test/java/com/yes25/yes255orderpaymentserver/presentation/controller/PolicyControllerTest.java
@@ -0,0 +1,22 @@
+package com.yes25.yes255orderpaymentserver.presentation.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PolicyControllerTest {
+
+    @DisplayName("")
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+}
